5f91b95c66940550088141e241608116cea744f3
Hajime Nakagami <nakagami@gmail.com>
2025-04-06 11:21:28 +0900

    a bit modify log.debug()

--- rdpy/protocol/rdp/tpkt.py
+++ rdpy/protocol/rdp/tpkt.py
@@ -213,7 +213,7 @@ class TPKT(RawLayer, IFastPathSender):
         @summary: Send encompassed data
         @param message: {network.Type} message to send
         """
-        log.debug("TPKT.send()")
+        log.debug(f"TPKT.send() {sizeof(message)=}")
         RawLayer.send(self, (UInt8(Action.FASTPATH_ACTION_X224), UInt8(0), UInt16Be(sizeof(message) + 4), message))
         
     def sendFastPath(self, secFlag, fastPathS):
